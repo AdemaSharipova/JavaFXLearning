fb6dddf07777d3f1c7f3135c7ccf6f59186200f0
Adema <adema.sharipova@inbox.ru>
2024-05-31 16:44:28 +0500

    Added homework 13

--- src/main/java/com/example/javafxdemo/Controller.java
+++ src/main/java/com/example/javafxdemo/Controller.java
@@ -4,10 +4,7 @@ package com.example.javafxdemo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,29 +20,63 @@ public class Controller implements Initializable {
     @FXML
     ToggleGroup answers;
 
-    private String question = "Your favourite fruit?";
-
-    private String[] variants = {
-            "Banana", "Orange", "Apple", "Pineapple"
+    private String[] questions = {
+            "What is the capital of France?",
+            "Which city is the capital of Australia?",
+            "What is the capital of Canada?",
+            "Which of these is the capital of Egypt?",
+            "What city is the capital of Spain?"
     };
 
-    private String correctVariant = "Apple";
+    private String[][] variants = {
+            {"Rome", "London", "Paris", "Berlin"},
+            {"Sydney", "Melbourne", "Canberra", "Brisbane"},
+            {"New York", "Ottawa", "Toronto", "Montreal"},
+            {"Cairo", "Istanbul", "Dubai", "Jerusalem"},
+            {"Madrid", "Lisbon", "Barcelona", "Seville"}
+    };
+
+    private int[] correctAnswers = {2, 2, 1, 0, 0};
+
+    private int currIndex = 0;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        questionArea.setText(question);
-        answer1.setText(variants[0]);
-        answer2.setText(variants[1]);
-        answer3.setText(variants[2]);
-        answer4.setText(variants[3]);
+        displayQuestion(currIndex);
     }
 
+    private void displayQuestion(int index) {
+        questionArea.setText(questions[index]);
+        answer1.setText(variants[index][0]);
+        answer2.setText(variants[index][1]);
+        answer3.setText(variants[index][2]);
+        answer4.setText(variants[index][3]);
+    }
+
+
+
     public void tryToAnswer(ActionEvent actionEvent) {
-        if (((RadioButton)answers.getSelectedToggle()).getText().equals(correctVariant)) {
+        RadioButton selectedRadioButton = (RadioButton) answers.getSelectedToggle();
+        int selectedIndex = -1;
+        if (selectedRadioButton == answer1) selectedIndex = 0;
+        else if (selectedRadioButton == answer2) selectedIndex = 1;
+        else if (selectedRadioButton == answer3) selectedIndex = 2;
+        else if (selectedRadioButton == answer4) selectedIndex = 3;
+
+        if (selectedIndex == correctAnswers[currIndex]) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Correct");
             alert.showAndWait();
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Wrong");
+            alert.showAndWait();
+        }
+
+        currIndex++;
+        if (currIndex < questions.length) {
+            displayQuestion(currIndex);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "End");
             alert.showAndWait();
         }
     }
